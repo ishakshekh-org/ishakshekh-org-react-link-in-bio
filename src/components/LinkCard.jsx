a86e0e0c93ff98d7003a7feee34768476ed5d9af
Ishak Shekh <113578052+ishakshekh-org@users.noreply.github.com>
2023-10-26 17:57:07 +0530

    increases code efficiency

--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -1,26 +1,34 @@
-import { github, linkedin, sushi } from "../assets/icons/index";
+import { linkData } from "../constants";
 import ConnectLink from "../sections/ConnectLink";
+
 
 const LinkCard = () => {
   return (
     <>
       <div className="flex flex-1 justify-center items-center  my-3">
         <ul className="w-full ">
-          <ConnectLink
-            img={github}
-            src={"https://github.com/ishakshekh-org"}
-            site={"GitHub"}
-          />
-          <ConnectLink
-            img={linkedin}
-            src={"https://www.linkedin.com/in/ishak-shekh-12bb94247/"}
-            site={"LinkedIn"}
-          />
-          <ConnectLink
-            img={sushi}
-            src={"https://sushi-boy.vercel.app/"}
-            site={"Sushi Boy"}
-          />
+          {/* <>
+            <ConnectLink
+              img={github}
+              src={"https://github.com/ishakshekh-org"}
+              site={"GitHub"}
+            />
+            <ConnectLink
+              img={linkedin}
+              src={"https://www.linkedin.com/in/ishak-shekh-12bb94247/"}
+              site={"LinkedIn"}
+            />
+            <ConnectLink
+              img={sushi}
+              src={"https://sushi-boy.vercel.app/"}
+              site={"Sushi Boy"}
+            />
+          </> */}
+          {
+            linkData.map(item => (
+              <ConnectLink img={item.img} src={item.src} site={item.site} key={item} />
+            ))
+          }
         </ul>
       </div>
     </>
--- src/components/NavPart.jsx
+++ src/components/NavPart.jsx
@@ -1,70 +1,38 @@
+import { data } from "../constants";
 import Navbar from "../sections/Navbar";
 
-import {
-  facebook,
-  instagram,
-  snapchat,
-  spotify,
-  twiiter,
-} from "../assets/icons";
-
-const data = [
-{
-    img: instagram,
-    src: "https://www.instagram.com/ishak_shekh_/"
-} ,
-{
-    img: facebook,
-    src: "https://www.facebook.com/profile.php?id=100085857733469"
-} ,
-{
-    img: snapchat,
-    src: "https://www.snapchat.com/add/its_ishak90?share_id=HIfk2J1yQpU&locale=en-IN"
-}, 
-{
-    img: spotify,
-    src: "https://open.spotify.com/user/31gf5qq4i4n2rtvwbar6n7ilpxk4?si=L7WLjRfDRkSQIF1i0T9Quw&nd=1"
-}, 
-{
-    img: twitter,
-    src: "https://twitter.com/Ishak_Shekh_?t=fa_QxmMpaQknqGntVEgZww&s=09"
-}, 
-
-
-];
 
 const NavPart = () => {
   return (
     <div className="flex justify-center items-center sm:w-fit m-auto my-3 p-4 sm:px-5 bg-[#e6e9f3]  border-2 shadow-inner rounded-full ">
       <ul className="flex justify-evenly w-full sm:justify-evenly gap-3">
-{/*
-        <Navbar img={instagram} src="https://www.instagram.com/ishak_shekh_/" />
-        <Navbar
-          img={facebook}
-          src="https://www.facebook.com/profile.php?id=100085857733469"
-          name={"facebook"}
-        />
+        {/* <>
+          <Navbar img={instagram} src="https://www.instagram.com/ishak_shekh_/" />
 
-        <Navbar
-          img={snapchat}
-          src="https://www.snapchat.com/add/its_ishak90?share_id=HIfk2J1yQpU&locale=en-IN"
-        />
-        <Navbar
-          img={twiiter}
-          src="https://twitter.com/Ishak_Shekh_?t=fa_QxmMpaQknqGntVEgZww&s=09"
-        />
-        <Navbar
-          img={spotify}
-          src="https://open.spotify.com/user/31gf5qq4i4n2rtvwbar6n7ilpxk4?si=L7WLjRfDRkSQIF1i0T9Quw&nd=1"
-        />
-
-*/}
-
-    {data.map(item => (
           <Navbar
-            img={item.img} src={item.src}
-        />
-))}
+            img={facebook}
+            src="https://www.facebook.com/profile.php?id=100085857733469"
+          />
+
+          <Navbar
+            img={snapchat}
+            src="https://www.snapchat.com/add/its_ishak90?share_id=HIfk2J1yQpU&locale=en-IN"
+          />
+          <Navbar
+            img={twiiter}
+            src="https://twitter.com/Ishak_Shekh_?t=fa_QxmMpaQknqGntVEgZww&s=09"
+          />
+          <Navbar
+            img={spotify}
+            src="https://open.spotify.com/user/31gf5qq4i4n2rtvwbar6n7ilpxk4?si=L7WLjRfDRkSQIF1i0T9Quw&nd=1"
+          />
+        </> */}
+        {
+          data.map(item => (
+            <Navbar img={item.img} src={item.src} key={item} />
+          ))
+        }
+
       </ul>
     </div>
   );
