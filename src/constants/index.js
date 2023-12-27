1c1e5438a3708fc402bc381c5cc3870d5fe3e3ff
Ishak Shekh <113578052+ishakshekh-org@users.noreply.github.com>
2023-12-27 18:59:52 +0530

    Add files via upload

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,54 +1,57 @@
 import {
-	facebook,
-	instagram,
-	snapchat,
-	spotify,
-	twiiter,
-	github,
-	linkedin,
-	sushi
+  facebook,
+  instagram,
+  snapchat,
+  spotify,
+  twiiter,
+  github,
+  linkedin,
+  sushi,
+  quote,
 } from "../assets/icons";
 
-
-
 export const data = [
-	{
-		img: instagram,
-		src: "https://www.instagram.com/ishak_shekh_/"
-	},
-	{
-		img: facebook,
-		src: "https://www.facebook.com/profile.php?id=100085857733469"
-	},
-	{
-		img: snapchat,
-		src: "https://www.snapchat.com/add/its_ishak90?share_id=HIfk2J1yQpU&locale=en-IN"
-	},
-	{
-		img: twiiter,
-		src: "https://twitter.com/Ishak_Shekh_?t=fa_QxmMpaQknqGntVEgZww&s=09"
-	},
-	{
-		img: spotify,
-		src: "https://open.spotify.com/user/31gf5qq4i4n2rtvwbar6n7ilpxk4?si=L7WLjRfDRkSQIF1i0T9Quw&nd=1"
-	},
-]
-
+  {
+    img: instagram,
+    src: "https://www.instagram.com/ishak_shekh_/",
+  },
+  {
+    img: facebook,
+    src: "https://www.facebook.com/profile.php?id=100085857733469",
+  },
+  {
+    img: snapchat,
+    src: "https://www.snapchat.com/add/its_ishak90?share_id=HIfk2J1yQpU&locale=en-IN",
+  },
+  {
+    img: twiiter,
+    src: "https://twitter.com/Ishak_Shekh_?t=fa_QxmMpaQknqGntVEgZww&s=09",
+  },
+  {
+    img: spotify,
+    src: "https://open.spotify.com/user/31gf5qq4i4n2rtvwbar6n7ilpxk4?si=L7WLjRfDRkSQIF1i0T9Quw&nd=1",
+  },
+];
 
 export const linkData = [
-	{
-		img: github,
-		src: "https://github.com/ishakshekh-org",
-		site: "GitHub"
-	},
-	{
-		img: linkedin,
-		src: "https://www.linkedin.com/in/ishak-shekh-12bb94247/",
-		site: "LinkedIn"
-	},
-	{
-		img: sushi,
-		src: "https://sushi-boy.vercel.app/",
-		site: "Sushi Boy"
-	}
-]
+  {
+    img: github,
+    src: "https://github.com/ishakshekh-org",
+    site: "GitHub",
+  },
+  {
+    img: linkedin,
+    src: "https://www.linkedin.com/in/ishak-shekh-12bb94247/",
+    site: "LinkedIn",
+  },
+  {
+    img: sushi,
+    src: "https://sushi-boy.vercel.app/",
+    site: "Sushi Boy",
+  },
+  {
+    img: quote,
+    src: "https://quotes-react-app-sigma.vercel.app/",
+    site: "Quotes App",
+  },
+];
